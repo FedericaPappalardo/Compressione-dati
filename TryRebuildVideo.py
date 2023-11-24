80834dbf70c92862c2dead65ec52adf69d4bea48
cfederico14 <carminefederico98.cf@gmail.com>
2023-11-24 16:47:32 +0100

    re-

--- TryRebuildVideo.py
+++ TryRebuildVideo.py
@@ -1,35 +1,3 @@
-'''import cv2
-import os
-
-# Percorso della cartella contenente i frame del video
-frame_folder_path = "C:\\Users\\carmi\\Desktop\\Compressione dati\\frame_modificati"
-
-# Percorso del file audio del video
-audio_file_path = "C:\\Users\\carmi\\Desktop\\Compressione dati\\Estratto_audio_video\\audio.mp3"
-
-# Output: Percorso e nome del video da generare
-output_video_path = "C:\\Users\\carmi\\Desktop\\Compressione dati\\video_finale.mp4"
-
-# Inizializza il VideoWriter per scrivere il nuovo video
-frame = cv2.imread(os.path.join(frame_folder_path, os.listdir(frame_folder_path)[0]))
-height, width, layers = frame.shape
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-video_writer = cv2.VideoWriter(output_video_path, fourcc, 30, (width, height))
-
-# Leggi ogni frame dalla cartella e scrivilo nel video
-for filename in sorted(os.listdir(frame_folder_path)):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        frame_path = os.path.join(frame_folder_path, filename)
-        frame = cv2.imread(frame_path)
-        video_writer.write(frame)
-
-# Rilascia le risorse del VideoWriter
-video_writer.release()
-
-# Aggiungi l'audio al video
-os.system(f"ffmpeg -i {output_video_path} -i {audio_file_path} -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 {output_video_path}_with_audio.mp4")
-'''
-
 import subprocess
 import os
 
