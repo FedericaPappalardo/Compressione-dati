59760bbecac36ed576fbec4c6a31d5ef90a5a5bd
dancol00 <danielecolucci2000@gmail.com>
2023-11-27 18:48:19 +0100

    Rimossa opzione -vf, che non fa niente nel nostro caso. Aggiunta funzione get_framerate necessaria alla ricostruzione del video finale

--- TryFFMPEG.py
+++ TryFFMPEG.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
-video_input = 'C:\\Users\\carmi\\Downloads\\prova.mp4'
-output_folder ='C:\\Users\\carmi\\Desktop\\Compressione dati'
+#video_input = 'C:\\Users\\carmi\\Downloads\\prova.mp4'
+#output_folder ='C:\\Users\\carmi\\Desktop\\Compressione dati'
+
 def estrai_audio(video_input, output_folder):
     # Crea una sottocartella per contenere l'audio
     output_subfolder = os.path.join(output_folder, 'Estratto_audio_video')
@@ -10,7 +11,8 @@ def estrai_audio(video_input, output_folder):
     output_path = os.path.join(output_subfolder, 'audio.mp3')
 
     comando = [
-        'C:\\Users\\carmi\\Desktop\\Compressione dati\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\bin\\ffmpeg.exe',
+        #'C:\\Users\\carmi\\Desktop\\Compressione dati\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\bin\\ffmpeg.exe',
+        'ffmpeg',
         '-i', video_input,
         '-vn',  # Specifica che non vuoi video, solo audio
         '-acodec', 'libmp3lame',  # Specifica il codec audio per il salvataggio come MP3
@@ -27,17 +29,31 @@ def dividere_in_frame(video_input, output_folder, output_pattern='frame_%04d.jpg
     output_subfolder = os.path.join(output_folder, 'frame_images')
     os.makedirs(output_subfolder, exist_ok=True)
 
+    # Si dovrebbero eliminare i file nella cartella giá presenti
+
     output_path = os.path.join(output_subfolder, output_pattern)
 
     comando = [
-        'C:\\Users\\carmi\\Desktop\\Compressione dati\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\ffmpeg-2023-11-22-git-0008e1c5d5-full_build\\bin\\ffmpeg.exe',
+        'ffmpeg',
         '-i', video_input,
-        '-vf', f'fps={fps}',
         '-q:v', '2',
         output_path
     ]
 
     subprocess.run(comando)
 
+def get_framerate(video_input):
+    comando = [
+        'ffprobe',
+        '-v', '0',
+        '-of', 'csv=p=0',
+        '-select_streams',
+        'v:0',
+        '-show_entries', 'stream=r_frame_rate',
+        video_input
+    ]
+
+    subprocess.run(comando)
+
 # Esempio di utilizzo
-dividere_in_frame(video_input, output_folder, fps=1)
+# dividere_in_frame(video_input, output_folder, fps=1)
