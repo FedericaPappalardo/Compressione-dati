1c1b5932474ef91977eacd117192324363e2ae41
dancol00 <danielecolucci2000@gmail.com>
2023-12-05 18:10:04 +0100

    Fix per frame dove non ci sono facce

--- TryMediapipe.py
+++ TryMediapipe.py
@@ -5,7 +5,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 my_drawing_specs = mp_drawing.DrawingSpec(color = (0, 255, 0), thickness = 1)
-video_input = "PATH_TO_INPUT_VIDEO"
+video_input = "INPUT_VIDEO_PATH"
 cap = cv2.VideoCapture(video_input)
 if cap.isOpened() == False:
     print("Errore nell'apertura del video")
@@ -13,8 +13,8 @@ if cap.isOpened() == False:
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
-video_output = "PATH_TO_OUTPUT_VIDEO"
-out = cv2.VideoWriter(video_output, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 60, (frame_width, frame_height))
+video_output = "OUTPUT_VIDEO_PATH"
+out = cv2.VideoWriter(video_output, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
 
 mp_face_mesh = mp.solutions.face_mesh
 
@@ -34,27 +34,25 @@ with mp_face_mesh.FaceMesh(
             
         results = face_mesh.process(image)
 
-        if not results.multi_face_landmarks:
-            continue
-        
-        for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(
-                image = image,
-                landmark_list = face_landmarks,
-                connections = mp_face_mesh.FACEMESH_TESSELATION,
-                landmark_drawing_spec = None,
-                connection_drawing_spec = mp_drawing_styles
-                .get_default_face_mesh_tesselation_style()
-            )
-            
-            mp_drawing.draw_landmarks(
-                image = image,
-                landmark_list = face_landmarks,
-                connections = mp_face_mesh.FACEMESH_CONTOURS,
-                landmark_drawing_spec = None,
-                connection_drawing_spec = my_drawing_specs
-#                 .get_default_face_mesh_contours_style()
-            )
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                    image = image,
+                    landmark_list = face_landmarks,
+                    connections = mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec = None,
+                    connection_drawing_spec = mp_drawing_styles
+                    .get_default_face_mesh_tesselation_style()
+                )
+
+                mp_drawing.draw_landmarks(
+                    image = image,
+                    landmark_list = face_landmarks,
+                    connections = mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec = None,
+                    connection_drawing_spec = my_drawing_specs
+    #                 .get_default_face_mesh_contours_style()
+                )
 
 
         #cv2.imshow("My video capture", cv2.flip(image, 1))
