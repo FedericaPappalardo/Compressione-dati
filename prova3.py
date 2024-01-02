faef2f664af0a9edf7da8f083dfe0a903faac2a3
Federica Pappalardo <75440758+federicapappalardo26@users.noreply.github.com>
2024-01-02 20:12:03 +0100

    Altra versione swapping volto
    Codice swap da cui sono partita https://matthewearl.github.io/2015/07/28/switching-eds-with-python/

---
+++ prova3.py
@@ -0,0 +1,51 @@
+import face_recognition
+from PIL import Image, ImageDraw, ImageFilter
+import numpy as np
+import os
+
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file("biden.jpg")
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+known_face_names = [
+    "Barack Obama",
+    "Joe Biden"
+]
+
+
+def Riconosci_Volto(image_path):
+        
+        # Load an image with an unknown face
+        unknown_image = face_recognition.load_image_file(image_path)
+        mylist=[]
+        # Find all the faces and face encodings in the unknown image
+        face_locations = face_recognition.face_locations(unknown_image)
+        face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+
+        # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+        # See http://pillow.readthedocs.io/ for more about PIL/Pillow
+        pil_image = Image.fromarray(unknown_image)
+        # Create a Pillow ImageDraw Draw instance to draw with
+        draw = ImageDraw.Draw(pil_image)
+        #pil_image.show()
+        # Loop through each face found in the unknown image
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                mylist.append(name)
+        return mylist
