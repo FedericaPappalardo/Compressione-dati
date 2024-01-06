5fc812756d861226557dee4d4c2f107aaa4adc3e
dancol00 <danielecolucci2000@gmail.com>
2024-01-06 17:02:37 +0100

    Cambio mkv

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ def main(input_video):
     imm_sost_folder = os.path.join(os.path.dirname(input_video), "immagini_per_sostituzione")
     TrySub.esegui_sostituzione(frame_folder, training_folder, imm_sost_folder, sub_frame_folder)
 
-    audio_file = os.path.join(frame_folder, "audio.mp3")
+    audio_file = os.path.join(frame_folder, "audio.mkv")
     output_video = os.path.join(os.path.dirname(input_video), "output_video.mp4")
     framerate = TryFFMPEG.get_framerate(input_video)
 
